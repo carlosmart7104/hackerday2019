cd70b64b0638d13069e8ac9a3d47daaae54ae7fb
Vichy <vichyvaleria@gmail.com>
2019-09-02 20:51:06 -0500

    menubar deslizable fallos

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,36 @@
 console.log()
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("js-menu").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("js-menu").style.width = "0%";
+}
+
+/* Open */
+function openNav() {
+  document.getElementById("js-menu").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("js-menu").style.height = "0%";
+}
+
+/* Open */
+function openNav() {
+  document.getElementById("js-menu").style.display = "block";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("js-menu").style.display = "none";
+}
+
+
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
