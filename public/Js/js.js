2de969494a91d92d021b7081ac68b69b706aac9e
Vichy <vichyvaleria@gmail.com>
2019-09-09 18:40:33 -0500

    se agrego conferencias

---
+++ public/Js/js.js
@@ -0,0 +1,29 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("js-menu").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("js-menu").style.width = "0%";
+}
+
+/* Open */
+function openNav() {
+  document.getElementById("js-menu").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("js-menu").style.height = "0%";
+}
+
+/* Open */
+function openNav() {
+  document.getElementById("js-menu").style.display = "block";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("js-menu").style.display = "none";
+}
