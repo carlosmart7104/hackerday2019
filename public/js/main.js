e6a02b277eb5d94ad0d33df76cdc8381e5610b04
Vichy <vichyvaleria@gmail.com>
2019-09-19 18:14:14 -0500

    cambios en el codigo de miguel ahora si ya todo bien

--- public/js/main.js
+++ public/js/main.js
@@ -8,8 +8,6 @@ navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
 });
 
-console.log()
-
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFsdml0Y2giLCJhIjoiY2p6dGw1a2IzMDZoazNjcDlocDhqNWM2bSJ9.bDEhLLP89f3bqEfFinF_0w';
 
 var map = new mapboxgl.Map({
