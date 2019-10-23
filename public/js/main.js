ef6e70d2d075fc92a30fb68938938cfe423f69ad
Carlos Martínez <carlosmart7104@gmail.com>
2019-10-22 20:01:23 -0500

    Se agregó el pdf de reporte 2019

--- public/js/main.js
+++ public/js/main.js
@@ -1,54 +1,53 @@
 console.log()
 
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+const mainNav = document.getElementById('js-menu')
+const navBarToggle = document.getElementById('js-navbar-toggle')
 
-navBarToggle.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('active');
-});
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active')
+})
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybG9zbWFydDcxMDQiLCJhIjoiY2p5b3Z4anp0MTk4azNqbzloZWZpd2R5bCJ9.zY0vP4G4sBCXxsVlKTPDaw';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiY2FybG9zbWFydDcxMDQiLCJhIjoiY2p5b3Z4anp0MTk4azNqbzloZWZpd2R5bCJ9.zY0vP4G4sBCXxsVlKTPDaw'
 
-var map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/carlosmart7104/ck11p5avf0ulw1cpcnb3b0t5o',
-	center: [-96.161224, 19.199093],
-	zoom: 14.75
-});
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/carlosmart7104/ck11p5avf0ulw1cpcnb3b0t5o',
+  center: [-96.161224, 19.199093],
+  zoom: 14.75
+})
 
-var marker = new mapboxgl.Marker()
+const marker = new mapboxgl.Marker()
   .setLngLat([-96.161224, 19.199093])
-  .addTo(map);
+  .addTo(map)
 
-var Swiper = new Swiper ('.swiper-container', {
-	slidesPerView: 3,
-	//centeredSlides: true,
-    //spaceBetween: 30,
-    // Optional parameters
-    //direction: 'horizontal',
-    loop: true,
+var Swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  // centeredSlides: true,
+  // spaceBetween: 30,
+  // Optional parameters
+  // direction: 'horizontal',
+  loop: true,
 
-    autoplay: {
-        delay: 3500,
-        disableOnInteraction: false,
-      },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false
+  },
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 
-    // And if we need scrollbar
-    //scrollbar: {
-    //  el: '.swiper-scrollbar',
-    //},
-  });
-
+  // And if we need scrollbar
+  // scrollbar: {
+  //  el: '.swiper-scrollbar',
+  // },
+})
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require('express');
 const path = require('path')
-const app = express();
+const express = require('express')
+const app = express()
 
 app.use(express.static('public'))
 
 app.get('/voluntarios', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/voluntarios.html'))
 })
+app.get('/reporte-hackerday2019', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/static/reporte-hackerday2019.pdf'))
+})
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Runing server on http://localhost:3000');
-});
+  console.log('Runing server on http://localhost:3000')
+})
